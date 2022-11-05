7ef05f799fe5d248ecb5e107b5f8ee7be2be0e69
nazmul7b7 <nazmul7b7@gmail.com>
2022-11-05 07:20:04 -0400

    add color trail to grid

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let color = 'black';
+
 function createGrid(num) {
     const container = document.querySelector('.container');
+
+    let cells = container.querySelectorAll('div');
+    cells.forEach((div) => div.remove());
 
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
@@ -7,8 +12,25 @@ function createGrid(num) {
     let amount = num * num;
     for(i = 0; i < amount; i++) {
         cell = document.createElement('div');
+        cell.addEventListener('mouseover', colorCell);
         container.appendChild(cell).className = 'cell';
     }
 }
 
-createGrid(16);
+function changeSize(input) {
+    if (input >= 2 || input <= 100) {
+        createGrid(input);
+    } else console.log("Too many Cells")
+}
+
+function colorCell() {
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
